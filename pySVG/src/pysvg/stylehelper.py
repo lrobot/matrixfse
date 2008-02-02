5b87c156b8416078490984a8551599918ebe1666
kerim.mansour <kerim.mansour@5cf2b410-d526-0410-a8a1-3763e5118393>
2008-02-02 15:35:37 +0000

    includes transforms

--- pySVG/src/pysvg/stylehelper.py
+++ pySVG/src/pysvg/stylehelper.py
@@ -18,91 +18,110 @@ working:
 
 """
 class StyleHelper:
-  """ Base class for all attributes related to styling
-  DEPRECATED: Atually replaced by a dictionary inside pysvg.
-  This will become a Helperclass like ObjectHelper
+  """ Helper Class to create a style_dict for those not familiar with svg attribute names.
   """
   def __init__(self):
     self.style_dict={}
 
-  # Font properties:
-  # tested
-  def setFontFamily(self, fontfamily="", fontsize="",fontstyle="",fontweight=""):
+  
+  # tested below
+  def setFontFamily(self, fontfamily):
     self.style_dict["font-family"]=fontfamily
+  
+  def setFontSize(self, fontsize):
     self.style_dict["font-size"]=fontsize
+  
+  def setFontStyle(self,fontstyle):
     self.style_dict["font-style"]=fontstyle
+  
+  def setFontWeight(self,fontweight):
     self.style_dict["font-weight"]=fontweight
     
     #tested
-  def setFilling(self, fill="",fillopacity="",fillrule=""):
+  def setFilling(self, fill):
     self.style_dict["fill"]=fill
+  
+  def setFillOpacity(self,fillopacity):
     self.style_dict["fill-opacity"]=fillopacity
+  
+  def setFillRule(self,fillrule):
     self.style_dict["fill-rule"]=fillrule
+  
+  def setStrokeWidth(self,strokewidth):
+    self.style_dict["stroke-width"]=strokewidth
+  def setStroke(self,stroke):
+    self.style_dict["stroke"]=stroke
     
-  def setStroke(self,strokewidth="",stroke="",strokeopacity="",strokemiterlimit="",strokelinejoin="",strikelinecap="",strokedashoffset="",strokedasharray=""):
+  
+  
+  #untested below  
+  def setStrokeDashArray(self,strokedasharray):
     self.style_dict["stroke-dasharray"]=strokedasharray
+  def setStrokeDashOffset(self,strokedashoffset):
     self.style_dict["stroke-dashoffset"]=strokedashoffset
+  def setStrokeLineCap(self,strikelinecap):
     self.style_dict["stroke-linecap"]=strikelinecap
+  def setStrokeLineJoin(self,strokelinejoin):
     self.style_dict["stroke-linejoin"]=strokelinejoin
+  def setStrokeMiterLimit(self,strokemiterlimit):
     self.style_dict["stroke-miterlimit"]=strokemiterlimit
+  def setStrokeOpacity(self,strokeopacity):
     self.style_dict["stroke-opacity"]=strokeopacity
-    self.style_dict["stroke-width"]=strokewidth #tested
-    self.style_dict["stroke"]=stroke #tested
     
 
   #is used to provide a potential indirect value (currentColor) for the 'fill', 'stroke', 'stop-color' properties.
-  def setCurrentColor(self,color):
+  def setCurrentColor(self, color):
     self.style_dict["color"]=color
      
    
   # Gradient properties:
-  def setGradient(self,stopcolor="",stopopacity=""):
+  def setStopColor(self, stopcolor):
     self.style_dict["stop-color"]=stopcolor
-    self.style_dict["stop-opacity"]=stopopacity
-      
   
+  def setStopOpacity(self, stopopacity):
+    self.style_dict["stop-opacity"]=stopopacity
 
-  def setRendering(colorrendering="",imagerendering="",shaperendering="", textrendering=""):
+  #rendering properties
+  def setColorRendering(self, colorrendering):
     self.style_dict["color-rendering"]=colorrendering
+   
+  def setImageRendering(self, imagerendering):
     self.style_dict["image-rendering"]=imagerendering
+    
+  def setShapeRendering(self, shaperendering):
     self.style_dict["shape-rendering"]=shaperendering
+    
+  def setTextRendering(self, textrendering):
     self.style_dict["text-rendering"]=textrendering
     
-  def setSolidProperties(self,solidcolor="",solidopacity=""):
+  def setSolidColor(self,solidcolor):
     self.style_dict["solid-color"]=solidcolor
+  
+  def setSolidOpacity(self, solidopacity):
     self.style_dict["solid-opacity"]=solidopacity
   
-  def setViewPort(self,vectoreffect="",viewportfill="",viewportfillopacity=""):
+  #Viewport properties  
+  def setVectorEffect(self, vectoreffect):
     self.style_dict["vector-effect"]=vectoreffect
+    
+  def setViewPortFill(self, viewportfill):
     self.style_dict["viewport-fill"]=viewportfill
+    
+  
+  def setViewPortOpacity(self, viewportfillopacity):
     self.style_dict["viewport-fill_opacity"]=viewportfillopacity
           
   # Text properties
-  def setTextProperties(self,displayalign="",lineincrement="",textanchor=""):
+  def setDisplayAlign(self,displayalign):
     self.style_dict["display-align"]=displayalign
+    
+  def setLineIncrement(self, lineincrement):
     self.style_dict["line-increment"]=lineincrement
+    
+  def setTextAnchor(self, textanchor):
     self.style_dict["text-anchor"]=textanchor
 
   def getStyleDict(self):
     return self.style_dict
   
-  """
-  def getXML(self):
-    count=0;
-    xml="style="
-    for key in self.style_dict.keys():
-      if self.style_dict.get(key)=="":
-        continue
-      if count>0:
-        xml+='; '
-      else:
-        xml+='"'
-      xml+='%s:%s' %(key,self.style_dict.get(key))
-      count+=1
-    xml+='" '
-    if len(xml)>8:
-      return xml
-    else: #empty style
-      return ""
-  """  
     
