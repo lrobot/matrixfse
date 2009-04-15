0b3de09ea31045b566de47747491e169d8b41fb2
kerim.mansour@gmail.com <kerim.mansour@gmail.com@5cf2b410-d526-0410-a8a1-3763e5118393>
2009-04-15 18:14:42 +0000

    bleeding edge inserted

---
+++ pySVG/bleedingEdge/src/testManualAssembly.py
@@ -0,0 +1,29 @@
+from pysvg import *
+'''
+Created on 15.04.2009
+
+@author: kerim
+'''
+s=svgType()
+s.set_height(300)
+s.set_width(400)
+l=lineType()
+l.set_x1(100)
+l.set_x2(200)
+l.set_y1(100)
+l.set_y2(200)
+l.set_stroke('#00C')
+l.set_stroke_width('2px')
+s.addElement(l)
+
+l=lineType()
+l.set_x1(210)
+l.set_x2(300)
+l.set_y1(200)
+l.set_y2(300)
+l.set_style("stroke:#00C;stroke-width:2px")
+s.addElement(l)
+
+f=open('c:\\test.svg','wb')
+#s.export(f, level=0, namespace_='',namespacedef_='xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"', name_='')
+s.exportLiteral(f, level=0, name_='')
