4a04b625e2a90dcb1f36e24d6026a507404fac83
kerim.mansour@gmail.com <kerim.mansour@gmail.com@5cf2b410-d526-0410-a8a1-3763e5118393>
2009-05-26 11:57:11 +0000

    deleted old code

--- pySVG/bleedingEdge/src/testXML.py
+++
@@ -1,6 +0,0 @@
-import pysvg as g
-fout=open('out.svg','wb')
-
-root=g.parse('TMs10kSVGDemo.svg')
-root.export(fout, 0, namespace_='',namespacedef_='xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"', name_='')
-fout.close()
